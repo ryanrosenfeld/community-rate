5c21f1d1d4cad38dfd540919dff52f52b0977f51
Glenna Manns <gmm6jd@virginia.edu>
2017-02-21 14:22:31 -0500

    Added create new user html page and php backend

--- init.php
+++ init.php
@@ -13,4 +13,5 @@
 	$db->query("CREATE TABLE Users (ID bigint primary key NOT NULL AUTO_INCREMENT, fb_ID bigint, Name char(50), Email char(35), DisplayName char(35), Password char(35))") or die ("Invalid create " . $db->error);
 	$db->query("CREATE TABLE Reviews (Movie_ID bigint(20), User_ID int(20), Rating float, Thoughts text, DateReviewed datetime DEFAULT CURRENT_TIMESTAMP)") or die ("Invalid create " . $db->error);
 	$db->query("CREATE TABLE Friendships (Follower bigint(20), Followee bigint(20), DateAdded datetime DEFAULT CURRENT_TIMESTAMP)");
+	echo "Setup Users, Reviews, Friendships tables in CommunityRate DB"
 ?>
