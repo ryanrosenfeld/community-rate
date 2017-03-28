bc4bf9a77fc576c7ac642c86e220df4eb7c4e4e4
ryanrosenfeld <rdr6re@virginia.edu>
2017-03-27 20:44:05 -0400

    Updated with freelancer theme, finished navbar

--- community_rate/general/templatetags/app_extras.py
+++ community_rate/general/templatetags/app_extras.py
@@ -2,7 +2,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('navbar.html')
+@register.inclusion_tag('navbar_og.html')
 def show_results(poll):
     choices = poll.choice_set.all()
     return {'choices': choices}
