63c119d2fbc93f29132924b3fabecf0da314d3d9
ryanrosenfeld <rdr6re@virginia.edu>
2017-05-28 21:36:03 -0400

    Incorporating Glenna's code

--- community_rate/users/views.py
+++ community_rate/users/views.py
@@ -10,8 +10,9 @@ from movies.models import Review, List
 from movies.services import get_movie_by_id
 from .forms import UpdateInfoForm
 
-def profile(request):
-    reviews = Review.objects.filter(user_id=request.user.id)
+
+def my_profile(request):
+    reviews = Review.objects.filter(creator_id=request.user.id)
     for r in reviews:
         r.movie = get_movie_by_id(r.movie_id, True)
     num_reviews = len(reviews)
@@ -35,8 +36,9 @@ def profile(request):
         request.user.about_me = request.POST.get('about_me', None)
         request.user.fav_quote = request.POST.get('fav_quote', None)
         request.user.save()
-    return render(request, 'my-profile.html', {'favorites': favorites, 'recents': recents, 'lists', 'page': 'profile',
-                                               'form': form, 'num_reviews': num_reviews, 'av_rating': av_rating})
+    return render(request, 'my-profile.html', {'favorites': favorites, 'recents': recents, 'lists': lists,
+											   'page': 'profile', 'form': form, 'num_reviews': num_reviews,
+											   'av_rating': av_rating})
 
 @login_required
 def profile(request, username=""):
@@ -54,20 +56,21 @@ def profile(request, username=""):
 	# Check if person requesting profile is the owner
 	owner = user == request.user
 
-	# Query reviews, length, ratings
+	# Query reviews, length, ratings, lists
 	reviews = Review.objects.filter(creator=request.user)
 	number = len(reviews)
 	avg = 0
 	for r in reviews:
-		movie =  get_movie_by_id(r.movie_id)
-		r.movie = movie
-		avg += movie.rating
-		
+		r.movie =  get_movie_by_id(r.movie_id, True)
+		avg += r.rating
+
 	if avg > 0:
 		avg /= number
+		avg = "{0:.1f}".format(avg)
 
 	favorites = sorted(reviews, key=lambda x: x.rating, reverse=True)
 	recents = sorted(reviews, key=lambda x: x.date_added, reverse=True)
+	lists = List.objects.filter(creator=request.user)
 
 	# Determine if this user is already following them OR
 	# request user is actually this user
@@ -75,16 +78,47 @@ def profile(request, username=""):
 	query = following.filter(following=user)
 	already_following = (len(query) > 0) | owner
 
-	if owner: 
+	# if owner of profile render my profile
+	if owner:
+		# Collect form for editing info
+		form = UpdateInfoForm(initial={'username': request.user.username, 'email': request.user.email,
+									   'first_name': request.user.first_name, 'last_name': request.user.last_name,
+									   'fav_quote': request.user.fav_quote, 'about_me': request.user.about_me})
 
-		return render(request, 'users/profile.html', {
-			'num_reviews': number, 
-			'avg_review': avg,
+		# If info updated make changes to user
+		if request.method == 'POST':
+			form = UpdateInfoForm(initial={'username': request.user.username, 'email': request.POST['email'],
+										   'first_name': request.POST['first_name'],
+										   'last_name': request.POST['last_name'],
+										   'fav_quote': request.POST['fav_quote'],
+										   'about_me': request.POST['about_me']})
+			# ADD IS_VALID CHECK?
+			request.user.email = request.POST['email']
+			request.user.first_name = request.POST['first_name']
+			request.user.last_name = request.POST['last_name']
+			request.user.about_me = request.POST.get('about_me', None)
+			request.user.fav_quote = request.POST.get('fav_quote', None)
+			request.user.save()
+
+		return render(request, 'users/my-profile.html', {
+			'num_reviews': number,
+			'av_rating': avg,
 			'favorites': favorites,
 			'recents': recents,
-			'page': "profile",
-			'already_following': already_following,
-			'user': user})
+			'page': 'profile',
+			'user': user,
+			'lists': lists,
+			'form': form})
+
+	# if other user render their public profile
+	return render(request, 'users/profile.html', {
+		'num_reviews': number,
+		'avg_review': avg,
+		'favorites': favorites,
+		'recents': recents,
+		'already_following': already_following,
+		'user': user,
+		'lists': lists})
 
 @login_required
 def settings(request):
