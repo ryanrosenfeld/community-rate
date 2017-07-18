6a3f7285fb384d800b4ca7ef331dfa3fe4214000
ryanrosenfeld <rdr6re@virginia.edu>
2017-07-17 23:14:49 -0400

    Changed pic storage to be by id

--- dev/general/admin.py
+++ dev/general/admin.py
@@ -4,3 +4,8 @@ from .models import *
 # Register your models here.
 admin.site.register(SiteUser)
 admin.site.register(Notification)
+
+
+class Users(admin.TabularInline):
+    model = SiteUser
+    readonly_fields = ('id',)
