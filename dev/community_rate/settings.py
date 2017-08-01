68ed38ec4c6eabc6d341920cf4b95080b6cfb25b
ryanrosenfeld <rdr6re@virginia.edu>
2017-08-01 18:10:38 -0400

    Switched back to gzip

--- dev/community_rate/settings.py
+++ dev/community_rate/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
