796c9496b899c6297e8389772b392da50b3eb9b5
ryanrosenfeld <rdr6re@virginia.edu>
2017-07-31 20:33:36 -0400

    Hopeful ipad fix

--- dev/users/ajax.py
+++ dev/users/ajax.py
@@ -84,7 +84,6 @@ def upload_profile_pic(request):
     if pic is None:
         return JsonResponse({})
 
-    print(pic)
     request.user.profile_pic = pic
     request.user.save()
     return JsonResponse({})
